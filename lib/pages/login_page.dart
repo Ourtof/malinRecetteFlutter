531997bf6ecb0e30a2d54dd1478a6b6e9bb5c90d
Ourtof <yann.eysseric@egs.school>
2025-11-05 23:20:52 +0100

    📦 NEW: page login

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,87 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  String? message;
+
+  Future<void> login() async {
+    // http.Client() => Prépare l’app pour les tests, perf, évolutions
+    final client = http.Client();
+    try {
+      final response = await client.post(
+        Uri.parse('http://127.0.0.1:8000/api/login'), // TODO: vérifier l'ip, la remplacer !
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          'email': emailController.text,
+          'password': passwordController.text,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        final token = data['token'];
+
+        setState(() {
+          message = 'Connexion réussie. Token : $token';
+        });
+
+        // TODO : stocker le token et rediriger
+      } else {
+        setState(() {
+          message = 'Erreur : ${response.statusCode} - ${response.body}';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        message = 'Erreur réseau : $e';
+      });
+    } finally {
+      client.close();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: passwordController,
+              decoration: const InputDecoration(labelText: 'Mot de passe'),
+              obscureText: true,
+            ),
+            const SizedBox(height: 16),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed('/register');
+              },
+              child: const Text("Pas de compte ? S'inscrire !"),
+            ),
+            ElevatedButton(onPressed: login, child: const Text('Se connecter')),
+            if (message != null) ...[
+              const SizedBox(height: 20),
+              Text(message!, style: const TextStyle(color: Colors.red)),
+            ],
+          ],
+        ),
+      ),
+    );
+  }
+}
