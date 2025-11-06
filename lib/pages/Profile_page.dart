49def39aa909381598a6a4b23448056fabec150a
Ourtof <yann.eysseric@egs.school>
2025-11-06 01:29:32 +0100

    profile page

---
+++ lib/pages/Profile_page.dart
@@ -0,0 +1,70 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  Map<String, dynamic>? user;
+  String? error;
+
+  @override
+  void initState() {
+    super.initState();
+    loadProfile();
+  }
+
+  Future<void> loadProfile() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('jwt_token');
+
+    if (token == null) {
+      setState(() => error = 'Utilisateur non connecté.');
+      return;
+    }
+
+    final response = await http.get(
+      Uri.parse(
+        'http://127.0.0.1:8000/api/me',
+      ), //TODO: À adapter selon l'IP réelle
+      headers: {'Authorization': 'Bearer $token'},
+    );
+
+    if (response.statusCode == 200) {
+      setState(() => user = jsonDecode(response.body));
+    } else {
+      setState(
+        () => error = 'Erreur : ${response.statusCode} — ${response.body}',
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Profil utilisateur')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: error != null
+            ? Text(error!, style: const TextStyle(color: Colors.red))
+            : user == null
+            ? const CircularProgressIndicator()
+            : Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Email : ${user!['email']}"),
+                  Text("Nom : ${user!['nom']}"),
+                  Text("Prénom : ${user!['prenom']}"),
+                  //TODO: vérifier si d'autres champs sont nécessaires
+                ],
+              ),
+      ),
+    );
+  }
+}
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:malinrecetteflutter/ui/widget/header_bar.dart';
-import 'package:malinrecetteflutter/ui/widget/header_content.dart';
 import 'package:malinrecetteflutter/ui/widget/footer_widget.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
