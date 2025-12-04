42e98d6a761b4abbcce3a7c1cd6b274326e7fc45
Ourtof <yann.eysseric@egs.school>
2025-12-04 03:22:38 +0100

    refacto

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -3,5 +3,5 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 class ApiConfig {
   static final String baseUrl = kIsWeb
       ? 'http://127.0.0.1:8000' // Flutter Web
-      : 'http://10.0.2.2:8000'; // Emulateur Android (plus tard)
+      : 'http://10.0.2.2:8000'; // Si émulateur Android
 }
