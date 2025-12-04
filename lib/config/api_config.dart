29d4e3577aecf227ee31627042b25e566f977ca8
Ourtof <yann.eysseric@egs.school>
2025-12-04 03:09:22 +0100

    👌 IMPROVE:  creation API_config.dart

---
+++ lib/config/api_config.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+class ApiConfig {
+  static final String baseUrl = kIsWeb
+      ? 'http://127.0.0.1:8000' // Flutter Web
+      : 'http://10.0.2.2:8000'; // Emulateur Android (plus tard)
+}
