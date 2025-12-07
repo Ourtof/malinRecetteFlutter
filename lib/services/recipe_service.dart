1f3f21ae9c820d18badfbd9e7a8dba8741f3625a
Ourtof <yann.eysseric@egs.school>
2025-12-07 19:40:59 +0100

    fix

--- lib/services/recipe_service.dart
+++ lib/services/recipe_service.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:typed_data';
 import 'package:http/http.dart' as http;
-import 'package:image_picker/image_picker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../models/recipe.dart';
