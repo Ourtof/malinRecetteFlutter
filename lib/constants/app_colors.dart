1b155a9558e7fdb632974896e649aac57d0a073c
yann <yann.eysseric@egs.school>
2026-01-06 17:37:00 +0100

    fix colors

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -4,7 +4,7 @@ class AppColors {
   static const Color neutral10 = Color(0xFFFFD5FF);
   static const Color neutral20 = Color(0xFFE6C79C);
   static const Color neutral60 = Color(0xFFF9564F);
-  static const Color neutral90 = Color(0xFFCA6A89);
+  static const Color neutral90 = Color(0xFF4B3B40);
   static const Color primary80 = Color(0xFF708D81);
   static const Color error = Color(0xFFFF0000);
 }
